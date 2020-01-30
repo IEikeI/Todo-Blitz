c7d4eebbc1afb38793b0f2c1491fa71e0b558689
Eike Karsten Schlicht <eike.karsten.schlicht@gmail.com>
2020-01-30 12:17:45 +0100

    - Added basic static view for "current todo-list" (just sample text) - Added switch + logic for enabling/disabling "dark mode"

--- Todo Blitz/ViewController.swift
+++ Todo Blitz/ViewController.swift
@@ -9,12 +9,42 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var darkModeSwitch: UISwitch!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func changeBackground(_ sender: Any) {
+        let everySubView = view.subviews
+        var everyUILabel = filteredUiLabels(everySubview: everySubView)
+       
+        if(darkModeSwitch.isOn) {
+            view.backgroundColor = UIColor.darkGray
+            changeTextColorOf(uiLabels: everyUILabel, toColor: UIColor.white)
+        } else {
+            view.backgroundColor = UIColor.white
+            changeTextColorOf(uiLabels: everyUILabel, toColor: UIColor.black)
+        }
+    }
+    
+    private func changeTextColorOf(uiLabels: Array<UILabel>, toColor: UIColor){
+        for eachLabel in uiLabels {
+            eachLabel.textColor = toColor
+        }
+    }
+    
+    private func filteredUiLabels(everySubview: Array<UIView>) -> Array<UILabel>{
+        var everyUILabel: Array<UILabel> = Array() // initialize array
+        for eachView in everySubview {
+                   if eachView is UILabel {
+                       let currentLabel = eachView as! UILabel
+                       everyUILabel.append(currentLabel)
+                   }
+               }
+        return everyUILabel
+    }
 }
 
