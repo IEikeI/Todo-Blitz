f9b3cae5570f88e7140df9530ae60d87d10a23ec
Eike Karsten Schlicht <eike.karsten.schlicht@gmail.com>
2020-01-30 10:38:58 +0100

    initial commit

---
+++ Todo Blitz/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Todo Blitz
+//
+//  Created by Eike Karsten Schlicht on 30.01.20.
+//  Copyright © 2020 Eike Karsten Schlicht. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
